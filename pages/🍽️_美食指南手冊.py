57280f7de40e5b72d0c32f994b65a790f4798078
liqi6 <s1143012@gm.ncue.edu.tw>
2024-12-18 19:08:21 +0800

    Update 🍽️_美食指南手冊.py

--- pages/🍽️_美食指南手冊.py
+++ pages/🍽️_美食指南手冊.py
@@ -8,7 +8,7 @@ st.markdown(
     """
 )
 
-st.header("美食指南手冊1.炭烤香料馬鈴薯章魚腳 Pulpo a la Gallega(Galician-style octopus)")
+st.header("1.炭烤香料馬鈴薯章魚腳 Pulpo a la Gallega(Galician-style octopus)")
 
 markdown = """
  源自西班牙西北部的加利西亞（Galicia）地區，加利西亞有著悠久的海鮮捕撈歷史，尤其是章魚，這使得章魚成為當地餐桌上的重要食材之一。而這道菜也是一種文化象徵，代表著加利西亞人對海洋資源和傳統烹飪的敬意。例如，在加利西亞的章魚節（Festa do Pulpo），這道菜會被大規模製作和享用，成為當地居民和遊客共同慶祝的中心。
