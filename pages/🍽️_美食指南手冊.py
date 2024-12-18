f28d64d23d4456091d7a86da1d05de8c4d8926c1
liqi6 <s1143012@gm.ncue.edu.tw>
2024-12-18 19:02:12 +0800

    Update 🍽️_美食指南手冊.py

--- pages/🍽️_美食指南手冊.py
+++ pages/🍽️_美食指南手冊.py
@@ -1,5 +1,26 @@
 import streamlit as st
 
+st.title("1.炭烤香料馬鈴薯章魚腳 Pulpo a la Gallega(Galician-style octopus)")
+
+st.markdown(
+    """
+   源自西班牙西北部的加利西亞（Galicia）地區，加利西亞有著悠久的海鮮捕撈歷史，尤其是章魚，這使得章魚成為當地餐桌上的重要食材之一。而這道菜也是一種文化象徵，代表著加利西亞人對海洋資源和傳統烹飪的敬意。例如，在加利西亞的章魚節（Festa do Pulpo），這道菜會被大規模製作和享用，成為當地居民和遊客共同慶祝的中心。
+這道菜的主要特色是將章魚腳和馬鈴薯結合，經過燒烤和香料調味，口感鮮嫩且極具層次感，通常作為前菜或共享菜品來享用，搭配著一杯清爽的白葡萄酒。
+    """
+)
+
+st.header("美食指南手冊")
+
+markdown = """
+1. For the [GitHub repository](https://github.com/opengeos/streamlit-map-template) or [use it as a template](https://github.com/opengeos/streamlit-map-template/generate) for your own project.
+2. Customize the sidebar by changing the sidebar text and logo in each Python files.
+3. Find your favorite emoji from https://emojipedia.org.
+4. Add a new app to the `pages/` directory with an emoji in the file name, e.g., `1_🚀_Chart.py`.
+
+"""
+
+st.markdown(markdown)
+
 #美食指南手冊
 ##西班牙
 #1.炭烤香料馬鈴薯章魚腳 Pulpo a la Gallega(Galician-style octopus)
